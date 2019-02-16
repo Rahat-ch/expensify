2959aadc8db6172ada359f35cf6a9d469c6a97a9
Rahatch <40510125+Rahat-ch@users.noreply.github.com>
2019-02-15 23:41:25 -0500

    deploying my second react app

---
+++ src/playground/es-6-arrow-function.js
@@ -0,0 +1,23 @@
+// const square = function (x){
+//     return x * x
+// }
+//
+// // const squareArrow = (x) => {
+// //     return x * x
+// // }
+//
+// const squareArrow = (x) => x * x;
+//
+// console.log(squareArrow(10));
+
+// fullname.split(" ")[0];
+
+const firstName = (str) => {
+    return str.split(" ")[0]
+};
+
+console.log(firstName("Rahat Chowdhury"));
+
+const firstName2 = (str) => str.split(" ")[0];
+
+console.log(firstName2("Hasiba Khan"));
---
+++ src/playground/es-6-let-const.js
@@ -0,0 +1,10 @@
+var nameVar = "Rahat";
+var nameVar = "Mike";
+console.log("namevar: " , nameVar);
+
+let nameLet = "Jen";
+nameLet = "Poop";
+console.log("nameLet", nameLet);
+
+const nameConst = "Frank";
+console.log("nameConst",nameConst);
